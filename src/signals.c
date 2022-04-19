ad75e3e5ad04a731656c869a3b82b003eb25bba0
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-04-19 22:05:17 +0200

    feat: add "CTRL-\"/SIGQUIT handler

--- src/signals.c
+++ src/signals.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <libft.h>
 
 void	sigint_h(int sig)
 {
@@ -12,7 +13,14 @@ void	sigint_h(int sig)
 	rl_redisplay();
 }
 
+void	sigquit_h(int sig)
+{
+	(void)sig;
+	ft_putstr_fd("\b\b  \b\b", STDOUT_FILENO);
+}
+
 void	init_signals(void)
 {
 	signal(SIGINT, sigint_h);
+	signal(SIGQUIT, sigquit_h);
 }
