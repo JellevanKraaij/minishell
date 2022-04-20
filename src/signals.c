7c2eed456e0aaa695ca1b9fd60955dd31ea21239
Jelle van Kraaij <55192476+JellevanKraaij@users.noreply.github.com>
2022-04-20 20:02:34 +0200

    fix: wrong behaviour CTRL-\(SIGQUIT) moves cursor to begin of line (#7)
    * refactor: improve code readability
    * fix: printf not interrupt safe

--- src/signals.c
+++ src/signals.c
@@ -4,23 +4,19 @@
 #include <readline/history.h>
 #include <libft.h>
 
-void	sigint_h(int sig)
+// handler for SIGQUIT signal (CTRL-C)
+// creates new prompt on next line
+static void	sigint_handler(int sig)
 {
 	(void)sig;
-	printf("\n");
+	ft_putchar_fd('\n', STDOUT_FILENO);
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
-void	sigquit_h(int sig)
-{
-	(void)sig;
-	ft_putstr_fd("\b\b  \b\b", STDOUT_FILENO);
-}
-
 void	init_signals(void)
 {
-	signal(SIGINT, sigint_h);
-	signal(SIGQUIT, sigquit_h);
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
 }
