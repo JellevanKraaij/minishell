e5f01f661487e41f7984d4cd51b71536b05be527
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-04-20 17:02:36 +0200

    refactor: improve code readability

--- src/signals.c
+++ src/signals.c
@@ -4,10 +4,12 @@
 #include <readline/history.h>
 #include <libft.h>
 
-void	sigint_h(int sig)
+// handler for SIGQUIT signal (CTRL-C)
+// creates new prompt on next line
+static void	sigint_handler(int sig)
 {
 	(void)sig;
-	printf("\n");
+	ft_putchar_fd('\n', STDOUT_FILENO);
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
@@ -15,6 +17,6 @@ void	sigint_h(int sig)
 
 void	init_signals(void)
 {
-	signal(SIGINT, sigint_h);
+	signal(SIGINT, sigint_handler);
 	signal(SIGQUIT, SIG_IGN);
 }
