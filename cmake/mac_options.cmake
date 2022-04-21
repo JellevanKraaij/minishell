f44ff00460eaf493a21303a46c88d6ca60075c27
Thirza-Os <74403468+Thirza-Os@users.noreply.github.com>
2022-04-21 12:30:32 +0200

    fix: :bug: Fix readline path mac

--- cmake/mac_options.cmake
+++ cmake/mac_options.cmake
@@ -1,4 +1,4 @@
 add_library(readline STATIC IMPORTED)
 set_property(TARGET readline APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
-set_target_properties(readline PROPERTIES IMPORTED_LOCATION_NOCONFIG "$ENV{HOME}/.brew/opt/readline/lib/libreadline.a")
-target_include_directories(readline INTERFACE "$ENV{HOME}/.brew/opt/readline/include")
+set_target_properties(readline PROPERTIES IMPORTED_LOCATION_NOCONFIG "/usr/local/opt/readline/lib/libreadline.a")
+target_include_directories(readline INTERFACE "/usr/local/Cellar/readline/8.1.2/include")
