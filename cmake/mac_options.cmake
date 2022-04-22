e7c25738534c843d4d564f88bbaccccf6a8bbb9f
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-04-22 17:26:22 +0200

    build: use brew prefix variable to make brew libraries compatible with 42 macs

--- cmake/mac_options.cmake
+++ cmake/mac_options.cmake
@@ -1,4 +1,6 @@
+execute_process(COMMAND brew --prefix OUTPUT_VARIABLE BREW_PREFIX)
+string(STRIP ${BREW_PREFIX} BREW_PREFIX)
 add_library(readline STATIC IMPORTED)
 set_property(TARGET readline APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
-set_target_properties(readline PROPERTIES IMPORTED_LOCATION_NOCONFIG "/usr/local/opt/readline/lib/libreadline.a")
-target_include_directories(readline INTERFACE "/usr/local/Cellar/readline/8.1.2/include")
+set_target_properties(readline PROPERTIES IMPORTED_LOCATION_NOCONFIG "${BREW_PREFIX}/opt/readline/lib/libreadline.a")
+target_include_directories(readline INTERFACE "${BREW_PREFIX}/opt/readline/include")
