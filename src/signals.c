8f0c8c130728ae317dca90b1d83f671b4bf99972
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-10-05 17:09:17 +0200

    fix: :bug: sigquit and sigint handling
    closes #88 closes #87

--- src/signals.c
+++ src/signals.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   signals.c                                          :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: jvan-kra/tosinga                             +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/10/04 17:45:02 by jvan-kra      #+#    #+#                 */
-/*   Updated: 2022/10/04 17:45:02 by jvan-kra      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tosinga <tosinga@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 17:45:02 by jvan-kra      #+  #+#    #+#             */
+/*   Updated: 2022/10/05 16:59:44 by tosinga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,38 @@
 static void	sigint_handler(int sig)
 {
 	(void)sig;
-	ft_putchar_fd('\n', STDOUT_FILENO);
+	ft_putchar_fd('\n', STDERR_FILENO);
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
 	g_last_exit_code = 1;
 }
 
+static void	sigint_incmd_handler(int sig)
+{
+	(void)sig;
+	ft_putchar_fd('\n', STDERR_FILENO);
+}
+
+static void	sigquit_handler(int sig)
+{
+	(void)sig;
+	ft_putstr_fd("Quit: 3", STDERR_FILENO);
+	ft_putchar_fd('\n', STDERR_FILENO);
+}
+
 void	enable_signals(int print_promt)
 {
 	if (print_promt)
+	{
 		signal(SIGINT, sigint_handler);
+		signal(SIGQUIT, SIG_IGN);
+	}
 	else
-		signal(SIGINT, SIG_IGN);
-	signal(SIGQUIT, SIG_IGN);
+	{
+		signal(SIGINT, sigint_incmd_handler);
+		signal(SIGQUIT, sigquit_handler);
+	}
 }
 
 void	disable_signals(void)
